680175155035fe70f4e916409ec2365fe166f18b
Zenon Harley <mobius@skythink.com>
2013-04-11 15:57:23 -0400

    Clean up and tweak xmobar config

--- etc/xmobar.hs
+++ etc/xmobar.hs
@@ -1,18 +1,32 @@
-Config { font = "-Misc-Fixed-Bold-R-Normal--13-120-75-75-C-70-ISO8859-1"
+-- RefreshRate is in 1/10 seconds
+Config { 
+       -- name of the font to be used
+       font = "-Misc-Fixed-Bold-R-Normal--13-120-75-75-C-70-ISO8859-1"
+       -- background color
        , bgColor = "black"
+       -- foreground color
        , fgColor = "grey"
-       , position = TopW L 10000000000 
+       -- screen position
+       , position = Top
+       -- send xmobar to the bottom of window stack
        , lowerOnStart = True
+       -- programs to run 
        , commands = [ Run Network "eth0" ["-L","0","-H","32","--normal","green","--high","red","-t","<rx> <tx>"] 5
                     , Run Network "wlan0" ["-L","0","-H","32","--normal","green","--high","red","-t","<rx> <tx>"] 5
-                    , Run Cpu ["-L","3","-H","50","--normal","green","--high","red","-t","<total>"] 10
-                    , Run Memory ["-t","<usedratio>%"] 10
-                    , Run Swap ["-t","<usedratio>"] 10
-                    , Run Date "%a %d-%b-%Y %l:%M%p" "date" 10
+                    , Run Cpu ["-b", ".", "-f", "*", "-L","3","-H","50","--normal","green","--high","red","-t","<bar>"] 5
+                    , Run TopProc ["-L","3","-H","30","--normal","green","--high","red","-t","<name1>"] 5
+                    , Run Memory ["-t","<usedratio>%"] 5
+                    , Run Swap ["-t","<usedratio>%"] 5
+                    , Run Date "%a %d-%b-%Y %l:%M:%S%p" "date" 10
                     , Run StdinReader
-                    , Run Com "acpi | awk '{ print $4 $5 }' | sed 's/,/ /'" [""] "batt" 10 
+                    , Run Com "if ping -c 1 8.8.8.8 > /dev/null; then echo '<fc=#3CB371>ok</fc>'; else echo '<fc=#FF8C00>no</fc>'; fi" [""] "status" 20
+                    , Run Com "acpi | awk '{ print $4 $5 }' | sed 's/,/ /'" [""] "batt" 50 
                     ]
+       -- character to be used for indicating commands in the output template
        , sepChar = "%"
+       -- 2-character alignment string, e.g. LEFT-ALIGNED } CENTERED { RIGHT-ALIGNED
        , alignSep = "}{"
-       , template = "%StdinReader% }{ %batt% | %cpu% [%memory%, %swap%] | %eth0% | %wlan0% | <fc=#ee9a00>%date%</fc>"
+       -- output template
+       -- StdinReader - Displays any text received by xmobar on its standard input.
+       , template = "%StdinReader% }{ %batt% <fc=#808080>|</fc> %eth0% <fc=#808080>|</fc> %wlan0% <fc=#808080>|</fc> %cpu% <fc=#808080>|</fc> %memory% <fc=#808080>|</fc> <fc=#DAA520>%date%</fc> <fc=#808080>|</fc> %status%"
       }
